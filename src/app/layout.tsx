83879d72d582be305e6fdc139d540e72629c1b0f
mohsinraz3 <mohsinraz3@gmail.com>
2025-05-27 17:40:33 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GridPattern } from "@/components/magicui/grid-pattern";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GridPattern className="z-0 blur-xs" />
         {children}
       </body>
     </html>
